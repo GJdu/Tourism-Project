f5bea8ae224123ea023033aeac88d49b38105bce
Brian <briandu8@gmail.com>
2021-06-08 14:50:18 +0200

    Updated deepface to preload models

--- deepFaceAnalysis.py
+++ deepFaceAnalysis.py
@@ -4,6 +4,18 @@ from deepface import DeepFace
 import pandas as pd
 import shutil
 
+
+age_model = DeepFace.build_model("Age")
+gender_model = DeepFace.build_model("Gender")
+race_model = DeepFace.build_model("Race")
+emotion_model = DeepFace.build_model("Emotion")
+
+models = {"age" : age_model,
+          "gender" : gender_model,
+          "race" : race_model,
+          "emotion" : emotion_model
+}
+
 def deepFaceAnalysis(detected_faces_path):
 
     age_list = []
@@ -14,7 +26,7 @@ def deepFaceAnalysis(detected_faces_path):
     # Pass all images through DeepFace
     for detected_face in glob.glob(detected_faces_path + '*.jpg'):
         image_face = cv2.imread(detected_face)
-        face_feature = DeepFace.analyze(image_face, actions = ['age', 'gender', 'race', 'emotion'], enforce_detection=False, )
+        face_feature = DeepFace.analyze(image_face, actions = ['age', 'gender', 'race', 'emotion'], models=models, enforce_detection=False)
         age_list.append(face_feature["age"])
         gender_list.append(face_feature["gender"])
         race_list.append(face_feature["dominant_race"])

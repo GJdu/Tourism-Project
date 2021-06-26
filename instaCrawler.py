3ca348be78d30baf20c5bacecfcefdf061792803
Brian <briandu8@gmail.com>
2021-06-26 12:06:53 +0200

    Added option to crawl multiple location

--- instaCrawler.py
+++ instaCrawler.py
@@ -1,11 +1,12 @@
 # https://github.com/realsirjoe/instagram-scraper
 
-def igramscraperAuthentication(instagram):
+import pandas as pd
+import instaAuthentication
 
-    import instaAuthentication
+def igramscraperAuthentication(instagram):
     username, passward = instaAuthentication.getLogin()
     instagram.with_credentials(username, passward, 'cache/')
-    instagram.login()
+    instagram.login(two_step_verificator=True)
 
     return instagram
 
@@ -22,9 +23,43 @@ def getLocationByID(instagram, location_id):
     location = instagram.get_location_by_id(location_id).slug
     return location
 
-def getMediaFromLocationID(instagram, location_id, count, b_top_media):
-    if b_top_media:
-        media = instagram.get_current_top_medias_by_location_id(location_id)
+
+def getLocationIdFromCSV(file):
+    location_df = pd.read_csv(file)
+    location_df.columns = ["location_id", "location_name"]
+
+    location_id = location_df["location_id"].tolist()
+    location_name = location_df["location_name"].tolist()
+
+    return location_id, location_name
+
+def getMediaFromLocationID(instagram, location_id_file = '',location_id = None, location_name = None,count=10, b_top_media=False):
+    if location_id_file:
+        media = []
+        location_id, location_name = getLocationIdFromCSV(location_id_file)
+        for ids in location_id:
+            if b_top_media:
+                media_obj = instagram.get_current_top_medias_by_location_id(ids)
+                for i in range(0, len(media_obj)):
+                    media_obj[i].location_id = location_id
+                    media_obj[i].location_name = location_name
+            else:
+                media_obj = instagram.get_medias_by_location_id(ids, count)
+                for i in range(0, len(media_obj)):
+                    media_obj[i].location_id = location_id
+                    media_obj[i].location_name = location_name
+            media.append(media_obj)
+    elif location_id:
+        if b_top_media:
+            media = instagram.get_current_top_medias_by_location_id(location_id)
+            for i in range(0, len(media)):
+                media[i].location_id = location_id
+                media[i].location_name = location_name
+        else:
+            media = instagram.get_medias_by_location_id(location_id, count)
+            for i in range(0, len(media)):
+                media[i].location_id = location_id
+                media[i].location_name = location_name
     else:
-        media = instagram.get_medias_by_location_id(location_id, count)
+        media = None
     return media

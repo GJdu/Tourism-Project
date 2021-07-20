b305e749bf497a65a1efbbfc8adf8a06b084d730
Brian <briandu8@gmail.com>
2021-07-20 10:40:21 +0200

    Update scrapeMediaWithUrls.py

--- Examples/scrapeMediaWithUrls.py
+++ Examples/scrapeMediaWithUrls.py
@@ -10,6 +10,8 @@ instagram = Instagram(sleep_between_requests=3)
 instaCrawler.igramscraperAuthentication(instagram, b_two_step_verificator=True)
 
 output_folder_path = ROOT_DIR + '/instaDataSample/'
+columns = TourismProject.getColumns()
 
 media = TourismProject.getMediaFromUrl(instagram=instagram, url='https://www.instagram.com/p/CRbOnd0hZHg/')
-TourismProject.processData(media=media, output_folder_path=output_folder_path)
+data = TourismProject.processData(media=media, output_folder_path=output_folder_path)
+TourismProject.dataFrameToCSV(data=data, columns=columns, output_folder_path=output_folder_path)
